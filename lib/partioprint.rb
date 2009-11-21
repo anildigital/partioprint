b1fbef3a1381d11ca7ee2e751b30ab62ac8722ee
arnabc <arnab.konnagar@gmail.com>
2009-11-22 01:25:15 +0800

    -- Added check to print the output only in development mode -- slightly modified the Comment structure

--- lib/partioprint.rb
+++ lib/partioprint.rb
@@ -4,7 +4,7 @@ module ActionView
     def render_partial_with_print(options = {})
       partial_name = options[:partial]
       result = render_partial_without_print(options)
-      result = "<!-- partial #{partial_name} starts -->\n" + result + "\n<!-- partial #{partial_name} ends -->" 
+      result = "<!-- ERB:START #{partial_name} -->\n" + result + "\n<!-- ERB:END #{partial_name} -->" if RAILS_ENV == 'development'
     end
     
     alias_method :render_partial_without_print, :render_partial
