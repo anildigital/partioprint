e15b9833b711fc5e59ed6e03630a6e05c5f48659
Anil Wadghule <anildigital@gmail.com>
2009-11-24 22:36:32 +0530

    Added regex so that it does not add comment before doctype declartion or html tag

--- lib/partioprint.rb
+++ lib/partioprint.rb
@@ -1,12 +1,17 @@
 module ActionView
   module Partials
-    
+
     def render_partial_with_print(options = {})
       partial_name = options[:partial]
       result = render_partial_without_print(options)
-      result = "<!-- ERB:START #{partial_name} -->\n" + result + "\n<!-- ERB:END #{partial_name} -->" 
+
+      if result !~ /^\s*<(!DOCTYPE|html)/
+        result = "<!-- ERB:START partial: #{partial_name} -->\n" + result + "\n<!-- ERB:END partial: #{partial_name} -->" 
+      end
+
+      return result
     end
-    
+
     alias_method :render_partial_without_print, :render_partial
     alias_method :render_partial, :render_partial_with_print
   end
